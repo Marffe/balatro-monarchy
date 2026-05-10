a775eeee01bd7527d5a6dd3167f286410f76aba4
Eremel <richardbailey@outlook.com>
2026-05-10 12:35:45 +0100

    Prismatic Windchime: adjust sound timing

--- content/jokers/windchime.lua
+++ content/jokers/windchime.lua
@@ -13,17 +13,28 @@ Monarchy.Joker({
         return {vars = {card.ability.extra.count, card.ability.extra.gain, card.ability.extra.xmult}}
     end,
     calculate = function(self, card, context)
-        if context.before then
-            for _, pcard in ipairs(context.scoring_hand) do
-                if pcard.ability.set == 'Enhanced' then
-                    card.ability.extra.current = card.ability.extra.current + 1
+        if context.individual and context.cardarea == G.play and context.other_card.ability.set == 'Enhanced' then
+            card.ability.extra.current = card.ability.extra.current + 1
+            G.E_MANAGER:add_event(Event({
+                func = function()
+                    card:juice_up()
+                    play_sound('monarchy_chime', math.random() + 1, 0.7 + (math.random(-3, 3)/10))
+                    return true
+                end
+            }))
+            return {
+                juice_card = context.other_card,
+                func = function()
                     if card.ability.extra.current == card.ability.extra.count then
-                        G.E_MANAGER:add_event(Event({
-                            func = function()
-                                play_sound('monarchy_chime')
-                                return true
-                            end
-                        }))
+                        for i=1, 5 do
+                            G.E_MANAGER:add_event(Event({
+                                type = 'after', delay = 0.4,
+                                func = function()
+                                    play_sound('monarchy_chime', math.random() + 1, 0.7 + (math.random(-3, 3)/10))
+                                    return true
+                                end
+                            }))
+                        end
                         SMODS.scale_card(card, {
                             ref_table = card.ability.extra,
                             ref_value = 'xmult',
@@ -33,7 +44,7 @@ Monarchy.Joker({
                         card.ability.extra.current = 0
                     end
                 end
-            end
+            }
         end
         if context.joker_main then
             return {
