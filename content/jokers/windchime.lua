e7dee42dd93fccfca35277337aa8b675b0d1279c
Eremel <richardbailey@outlook.com>
2026-05-15 22:45:49 +0100

    Windchime: doesn't increment on retriggers

--- content/jokers/windchime.lua
+++ content/jokers/windchime.lua
@@ -14,7 +14,7 @@ Monarchy.Joker({
         return {vars = {card.ability.extra.count, card.ability.extra.gain, card.ability.extra.xmult}}
     end,
     calculate = function(self, card, context)
-        if context.individual and context.cardarea == G.play and context.other_card.ability.set == 'Enhanced' then
+        if context.individual and context.cardarea == G.play and context.other_card.ability.set == 'Enhanced' and not context.other_card.repetition_trigger then
             card.ability.extra.current = card.ability.extra.current + 1
             G.E_MANAGER:add_event(Event({
                 func = function()
@@ -29,7 +29,7 @@ Monarchy.Joker({
                     if card.ability.extra.current == card.ability.extra.count then
                         for i=1, 5 do
                             G.E_MANAGER:add_event(Event({
-                                type = 'after', delay = 0.4,
+                                type = 'after', delay = 0.8,
                                 func = function()
                                     play_sound('monarchy_chime', math.random() + 1, 0.7 + (math.random(-3, 3)/10))
                                     return true
